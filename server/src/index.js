f45703c9f11820da99868113c36715e6ba67f727
Jubayer <aljubayer92@gmail.com>
2025-11-20 21:00:17 +0600

    feat(server): support DATA_DIR env for persisted storage

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({ origin: '*', methods: ['GET', 'POST'], credentials: false }));
 app.use(express.json());
 
-const dataDir = path.join(__dirname, '..', 'data');
+const dataDir = process.env.DATA_DIR ? process.env.DATA_DIR : path.join(__dirname, '..', 'data');
 const usersFile = path.join(dataDir, 'users.json');
 
 function ensureDataFiles() {
